5afad293f9072d1f8bfafaff904852f226dc4b98
liushuang <liu.shuang@linecorp.com>
2019-05-24 13:51:01 +0800

    add mybatisGenerator;change ppt

--- springboot/database-demo/src/main/java/org/newit/microservice/databasedemo/DatabaseDemoApplication.java
+++ springboot/database-demo/src/main/java/org/newit/microservice/databasedemo/DatabaseDemoApplication.java
@@ -1,11 +1,9 @@
 package org.newit.microservice.databasedemo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan(basePackages = "org.newit.microservice.databasedemo.dao.*")
 public class DatabaseDemoApplication {
 
 	public static void main(String[] args) {
