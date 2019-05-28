7bde9d0cb5c049e5f282d6d5348f59850f63347d
liushuang <525982193@qq.com>
2019-05-28 17:29:25 +0800

    add websocket demo

--- demo/springmvc-demo/src/main/java/org/newit/microservice/springmvc/MyTime.java
+++ demo/springmvc-demo/src/main/java/org/newit/microservice/springmvc/MyTime.java
@@ -6,7 +6,7 @@ public class MyTime {
     public Long getCurrentTime() {
         return currentTime;
     }
-    
+
     public void setCurrentTime(Long currentTime) {
         this.currentTime = currentTime;
     }
