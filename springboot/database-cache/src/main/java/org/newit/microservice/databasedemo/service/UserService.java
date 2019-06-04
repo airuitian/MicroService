5c7e38b4ab10879c31f37900935d2d461baeca0c
liushuang <525982193@qq.com>
2019-06-04 14:34:18 +0800

    update database demo

--- springboot/database-cache/src/main/java/org/newit/microservice/databasedemo/service/UserService.java
+++ springboot/database-cache/src/main/java/org/newit/microservice/databasedemo/service/UserService.java
@@ -1,12 +1,14 @@
 package org.newit.microservice.databasedemo.service;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
-import javax.annotation.PostConstruct;
-
+import org.checkerframework.checker.units.qual.A;
 import org.newit.microservice.databasedemo.dao.UserJdbcTmpl;
+import org.newit.microservice.databasedemo.dao.UserMapper;
 import org.newit.microservice.databasedemo.model.User;
+import org.newit.microservice.databasedemo.model.UserExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,9 @@ import com.google.common.cache.LoadingCache;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
 
     LoadingCache<Long, User> userCache = CacheBuilder.newBuilder()
             .expireAfterWrite(10, TimeUnit.SECONDS)
@@ -53,4 +58,8 @@ public class UserService {
         return userJdbcTmpl.getUserById(userId);
     }
 
+    public List<User> getUserList() {
+        UserExample example = new UserExample();
+        return userMapper.selectByExample(example);
+    }
 }
--- springboot/database-cache/src/test/java/org/newit/microservice/databasedemo/dao/UserMapperTest.java
+++ springboot/database-cache/src/test/java/org/newit/microservice/databasedemo/dao/UserMapperTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.newit.microservice.databasedemo.model.User;
+import org.newit.microservice.databasedemo.model.UserExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -30,7 +31,9 @@ public class UserMapperTest {
         user.setName("test from mybatis2");
         userMapper.insert(user);
 
-        User userFromDB = userMapper.selectById(user.getId());
+        UserExample example = new UserExample();
+        example.createCriteria().andIdEqualTo(user.getId());
+        User userFromDB = userMapper.selectByExample(example).get(0);
         Assert.assertNotNull(userFromDB);
         Assert.assertEquals(user.getName(), userFromDB.getName());
     }
