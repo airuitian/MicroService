c0970dd280e6db24ed61ae95254f691cb5a3bfa3
liushuang <525982193@qq.com>
2019-06-18 11:41:02 +0800

    add eureka-server security

---
+++ middlerware/eureka-server/src/main/java/org/newit/springcloud/eurekaserver/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package org.newit.springcloud.eurekaserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(NoOpPasswordEncoder.getInstance())
+            .withUser("newit").password("newit")
+            .authorities("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
